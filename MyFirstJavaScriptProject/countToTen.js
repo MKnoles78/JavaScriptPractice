9a5405ef85b9f5b104b63704d29cb7a5a9ee6639
Mike Knoles <MKnoles78@gmail.com>
2022-01-08 23:14:50 -0500

    working with alerts

--- MyFirstJavaScriptProject/countToTen.js
+++ MyFirstJavaScriptProject/countToTen.js
@@ -5,3 +5,8 @@ function countToTen() {
         document.getElementById("theCount").innerHTML += count + "<br>";
     }
 }
+
+var firstNumber = 3;
+var secondNumber = 7;
+var total = Number(firstNumber) + Number(secondNumber);
+alert (total);
